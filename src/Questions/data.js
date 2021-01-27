ab03b215937f7781df82b7186955bff27f6bcb68
seyoungjoo <sellyjphoto@gmail.com>
2021-01-27 19:13:30 +0900

    questions page

---
+++ src/Questions/data.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    id: 1,
+    title: '이 웹사이트의 개발자는 누구인가요?',
+    info:
+      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+  },
+  {
+    id: 2,
+    title: '사용된 기술스텍은 무엇인가요?',
+    info:
+      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+  },
+  {
+    id: 3,
+    title: 'What is BankID?',
+    info:
+      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+  },
+  {
+    id: 4,
+    title: 'Whose birth number can I use?',
+    info:
+      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+  }
+]
+export default questions
