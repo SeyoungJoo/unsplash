8d4b9eed0e11eec8764a88a890353db6363ccee5
seyoungjoo <sellyjphoto@gmail.com>
2021-01-29 00:44:41 +0900

    added css styling for slide

--- src/Review/Review.js
+++ src/Review/Review.js
@@ -4,8 +4,26 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function Review() {
-  const [people, setPeople] = useState(data)
-  const [index, setIndex] = useState(0)
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if(index < 0){
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex){
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(()=> {
+      setIndex(index + 1);
+    },3000)
+    return ()=> clearInterval(slider)
+  },[index]);
+
   return (
     <Wrapper>
       <section className="section">
@@ -17,25 +35,26 @@ function Review() {
         <div className="section-center">
           {people.map((person, personIndex)=> {
             const {id, image, name, title, quote} = person;
-            let position = 'nestSlide'
+            let position = 'nextSlide'
             if (personIndex === index) {
               position = 'activeSlide'
             }
-            if(personIndex = index - 1){
+            if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)){
               position = 'lastSlide'
             }
-           return <article key={id}>
+           return (
+           <article className={position} key={id}>
               <img src={image} alt={name} className='person-img'/>
               <h4>{name}</h4>
               <p className='title'>{title}</p>
               <p className="text">{quote}</p>
               <FaQuoteRight className='icon' />
-            </article>
+            </article>)
           })}
-          <button className='prev'>
+          <button className='prev' onClick={()=> setIndex(index - 1)}>
             <FiChevronLeft/>
           </button>
-          <button className='next'>
+          <button className='next' onClick={()=> setIndex(index + 1)}>
             <FiChevronRight/>
           </button>
 
@@ -46,138 +65,6 @@ function Review() {
 }
 
 const Wrapper = styled.section `
-:root {
-  /* dark shades of primary color*/
-  --clr-primary-1: hsl(21, 91%, 17%);
-  --clr-primary-2: hsl(21, 84%, 25%);
-  --clr-primary-3: hsl(21, 81%, 29%);
-  --clr-primary-4: hsl(21, 77%, 34%);
-  --clr-primary-5: hsl(21, 62%, 45%);
-  --clr-primary-6: hsl(21, 57%, 50%);
-  --clr-primary-7: hsl(21, 65%, 59%);
-  --clr-primary-8: hsl(21, 80%, 74%);
-  --clr-primary-9: hsl(21, 94%, 87%);
-  --clr-primary-10: hsl(21, 100%, 94%);
-  /* darkest grey - used for headings */
-  --clr-grey-1: hsl(209, 61%, 16%);
-  --clr-grey-2: hsl(211, 39%, 23%);
-  --clr-grey-3: hsl(209, 34%, 30%);
-  --clr-grey-4: hsl(209, 28%, 39%);
-  /* grey used for paragraphs */
-  --clr-grey-5: hsl(210, 22%, 49%);
-  --clr-grey-6: hsl(209, 23%, 60%);
-  --clr-grey-7: hsl(211, 27%, 70%);
-  --clr-grey-8: hsl(210, 31%, 80%);
-  --clr-grey-9: hsl(212, 33%, 89%);
-  --clr-grey-10: hsl(210, 36%, 96%);
-  --clr-white: #fff;
-  --clr-red-dark: hsl(360, 67%, 44%);
-  --clr-red-light: hsl(360, 71%, 66%);
-  --clr-green-dark: hsl(125, 67%, 44%);
-  --clr-green-light: hsl(125, 71%, 66%);
-  --clr-black: #222;
-  --transition: all 0.3s linear;
-  --spacing: 0.1rem;
-  --radius: 0.25rem;
-  --light-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
-  --dark-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
-  --max-width: 1170px;
-  --fixed-width: 620px;
-}
-/*
-=============== 
-Global Styles
-===============
-*/
-
-*,
-::after,
-::before {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  background: var(--clr-grey-10);
-  color: var(--clr-grey-1);
-  line-height: 1.5;
-  font-size: 0.875rem;
-}
-ul {
-  list-style-type: none;
-}
-a {
-  text-decoration: none;
-}
-h1,
-h2,
-h3,
-h4 {
-  letter-spacing: var(--spacing);
-  text-transform: capitalize;
-  line-height: 1.25;
-  margin-bottom: 0.75rem;
-}
-h1 {
-  font-size: 3rem;
-}
-h2 {
-  font-size: 2rem;
-}
-h3 {
-  font-size: 1.25rem;
-}
-h4 {
-  font-size: 0.875rem;
-}
-p {
-  margin-bottom: 1.25rem;
-  color: var(--clr-grey-5);
-}
-@media screen and (min-width: 800px) {
-  h1 {
-    font-size: 4rem;
-  }
-  h2 {
-    font-size: 2.5rem;
-  }
-  h3 {
-    font-size: 1.75rem;
-  }
-  h4 {
-    font-size: 1rem;
-  }
-  body {
-    font-size: 1rem;
-  }
-  h1,
-  h2,
-  h3,
-  h4 {
-    line-height: 1;
-  }
-}
-/*  global classes */
-
-/* section */
-.section {
-  width: 90vw;
-  margin: 5rem auto;
-  max-width: var(--max-width);
-}
-
-@media screen and (min-width: 992px) {
-  .section {
-    width: 95vw;
-  }
-}
-/*
-=============== 
-Slider
-===============
-*/
 .title {
   text-align: center;
   margin-bottom: 2rem;
@@ -274,6 +161,7 @@ article h4 {
     font-size: 1.5rem;
   }
 }
+
 article {
   position: absolute;
   top: 0;
@@ -292,6 +180,18 @@ article.lastSlide {
 }
 article.nextSlide {
   transform: translateX(100%);
+}
+
+.section {
+  width: 90vw;
+  margin: 5rem auto;
+  max-width: var(--max-width);
+}
+
+@media screen and (min-width: 992px) {
+  .section {
+    width: 95vw;
+  }
 }
 `
 export default Review;
